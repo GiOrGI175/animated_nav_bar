bfee535544c9b8bc3087d0d49d676787b91d25fa
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-07-21 23:18:01 +0400

    feature/aurora animation

--- components/header/SlideTabsExample.tsx
+++ components/header/SlideTabsExample.tsx
@@ -4,7 +4,7 @@ import SlideTabs from './SlideTabs';
 
 export default function SlideTabsExample() {
   return (
-    <div className='py-5 grid place-content-center bg-neutral-100 '>
+    <div className='py-5 grid place-content-center  bg-[#020617] '>
       <SlideTabs />
     </div>
   );
